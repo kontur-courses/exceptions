cefd6e33a47229004d4117b37339680cf40a766b
Pavel Egorov <xoposhiy@mail.ru>
2016-11-18 15:14:46 +0500

    task improvements Add real solution (not just hints in comments)

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -4,114 +4,120 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Xml.Serialization;
 using NLog;
 
 namespace Exceptions
 {
-    // Что тут не так?
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            try
-            {
-                var filenames = args;
-                var settings = LoadSettings();
-                ConvertFiles(filenames, settings);
-            }
-            catch (Exception e)
-            {
-                LogManager.GetCurrentClassLogger().Error(e);
-                Console.WriteLine(e.Message);
-            }
-        }
+	public class Program
+	{
+		private static readonly Logger log = LogManager.GetCurrentClassLogger();
 
-        private static void ConvertFiles(string[] filenames, Settings settings)
-        {
-            var threads = filenames
-                .Select(fn => new Thread(() => ConvertFile(fn, settings)))
-                .ToList();
-            threads.ForEach(t => t.Start());
-            threads.ForEach(t => t.Join());
-        }
+		public static void Main(string[] args)
+		{
+			try
+			{
+				var filenames = args.Any() ? args : new[] { "text.txt" };
+				var settings = LoadSettings();
+				ConvertFiles(filenames, settings);
+			}
+			catch (Exception e)
+			{
+				log.Error(e);
+			}
+		}
 
-        private static Settings LoadSettings()
-        {
-            var serializer = new XmlSerializer(typeof(Settings));
-            var content = File.ReadAllText("settings.xml");
-            return (Settings)serializer.Deserialize(new StringReader(content));
-        }
+		private static void ConvertFiles(string[] filenames, Settings settings)
+		{
+			var tasks = filenames
+				.Select(fn => Task.Run(() => ConvertFile(fn, settings)))
+				.ToArray();
+			Task.WaitAll(tasks);
+		}
 
-        private static void ConvertFile(string filename, Settings settings)
-        {
-            Thread.CurrentThread.CurrentCulture = new CultureInfo(settings.SourceCultureName);
-            if (settings.Verbose)
-            {
-                Console.WriteLine("Processing file " + filename);
-                Console.WriteLine("Source Culture " + Thread.CurrentThread.CurrentCulture.Name);
-            }
-            IEnumerable<string> lines;
-            try
-            {
-                lines = PrepareLines(filename);
-            }
-            catch
-            {
-                Console.WriteLine($"File {filename} not found");
-                return;
-            }
-            File.WriteAllLines(filename + ".out", lines.Select(ConvertLine));
-        }
+		private static Settings LoadSettings()
+		{
+			var serializer = new XmlSerializer(typeof(Settings));
+			var content = File.ReadAllText("settings.xml");
+			return (Settings)serializer.Deserialize(new StringReader(content));
+		}
 
-        private static IEnumerable<string> PrepareLines(string filename)
-        {
-            var lineIndex = 0;
-            foreach (var line in File.ReadLines(filename))
-            {
-                if (line == "") continue;
-                yield return line.Trim();
-                lineIndex++;
-            }
-            yield return lineIndex.ToString();
-        }
+		private static void ConvertFile(string filename, Settings settings)
+		{
+			Thread.CurrentThread.CurrentCulture = new CultureInfo(settings.SourceCultureName);
+			if (settings.Verbose)
+			{
+				log.Info("Processing file " + filename);
+				log.Info("Source Culture " + Thread.CurrentThread.CurrentCulture.Name);
+			}
+			IEnumerable<string> lines;
+			try
+			{
+				lines = PrepareLines(filename);
+			}
+			catch
+			{
+				log.Error($"File {filename} not found");
+				return;
+			}
+			var convertedLines = lines
+				.Select(ConvertLine)
+				.Select(s => s.Length + " " + s);
+			File.WriteAllLines(filename + ".out", convertedLines);
+		}
 
-        private static string ConvertLine(string arg)
-        {
-            try
-            {
-                return ConvertAsDateTime(arg);
-            }
-            catch
-            {
-                try
-                {
-                    return ConvertAsDouble(arg);
-                }
-                catch
-                {
-                    return ConvertAsCharIndexInstruction(arg);
-                }
-            }
-        }
+		private static IEnumerable<string> PrepareLines(string filename)
+		{
+			var lineIndex = 0;
+			foreach (var line in File.ReadLines(filename))
+			{
+				if (line == "") continue;
+				yield return line.Trim();
+				lineIndex++;
+			}
+			yield return lineIndex.ToString();
+		}
+
+		private static string ConvertLine(string arg)
+		{
+			try
+			{
+				return ConvertAsDateTime(arg);
+			}
+			catch
+			{
+				try
+				{
+					return ConvertAsDouble(arg);
+				}
+				catch
+				{
+					return ConvertAsCharIndexInstruction(arg);
+				}
+			}
+		}
 
 
-        private static string ConvertAsCharIndexInstruction(string s)
-        {
-            var parts = s.Split();
-            var charIndex = int.Parse(parts[0]);
-            var text = parts[1];
-            return text[charIndex].ToString();
-        }
+		private static string ConvertAsCharIndexInstruction(string s)
+		{
+			var parts = s.Split();
+			if (parts.Length < 2) return null;
+			var charIndex = int.Parse(parts[0]);
+			if (charIndex < 0 || charIndex >= parts[1].Length)
+				return null;
+			var text = parts[1];
+			return text[charIndex].ToString();
+		}
 
-        private static string ConvertAsDateTime(string arg)
-        {
-            return DateTime.Parse(arg).ToString(CultureInfo.InvariantCulture);
-        }
+		private static string ConvertAsDateTime(string arg)
+		{
+			return DateTime.Parse(arg).ToString(CultureInfo.InvariantCulture);
+		}
 
-        private static string ConvertAsDouble(string arg)
-        {
-            return double.Parse(arg).ToString(CultureInfo.InvariantCulture);
-        }
-    }
+		private static string ConvertAsDouble(string arg)
+		{
+			return double.Parse(arg).ToString(CultureInfo.InvariantCulture);
+		}
+	}
 }
--- Exceptions/Settings.cs
+++ Exceptions/Settings.cs
@@ -1,8 +1,21 @@
-﻿namespace Exceptions
+﻿using System;
+
+namespace Exceptions
 {
-    public class Settings
-    {
-        public string SourceCultureName;
-        public bool Verbose;
-    }
+	public class Settings
+	{
+		public static Settings Default = new Settings("ru", false);
+		public string SourceCultureName;
+		public bool Verbose;
+
+		public Settings()
+		{
+		}
+
+		private Settings(string sourceCultureName, bool verbose)
+		{
+			SourceCultureName = sourceCultureName;
+			Verbose = verbose;
+		}
+	}
 }
